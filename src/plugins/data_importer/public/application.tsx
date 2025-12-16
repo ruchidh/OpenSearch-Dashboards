1977ac21a06ef7f60fdb3bcf0cdcd1cb0033b485
Ruchi Sharma <ruchsh@amazon.com>
2025-12-15 23:41:14 -0800

    remove ununsed enhahnced app
    Signed-off-by: Ruchi Sharma <ruchsh@amazon.com>

--- src/plugins/data_importer/public/application.tsx
+++ src/plugins/data_importer/public/application.tsx
@@ -7,7 +7,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppMountParameters, CoreStart } from '../../../core/public';
 import { DataImporterPluginStartDependencies } from './types';
-import { EnhancedDataImporterPluginApp } from './components/enhanced_data_importer_app';
 import { RedesignedDataImporterPluginApp } from './components/redesigned_data_importer_app';
 import { PublicConfigSchema } from '../config';
 import { DataImporterPluginSetupDeps } from './types';
@@ -19,36 +18,19 @@ export const renderApp = (
   { dataSource, dataSourceManagement }: DataImporterPluginSetupDeps,
   config: PublicConfigSchema
 ) => {
-  // Use the redesigned UI by default, with fallback to enhanced version
-  const useRedesignedUI = config.useRedesignedUI !== false; // Default to true
-
   ReactDOM.render(
-    useRedesignedUI ? (
-      <RedesignedDataImporterPluginApp
-        basename={appBasePath}
-        notifications={notifications}
-        http={http}
-        application={application}
-        navigation={navigation}
-        config={config}
-        savedObjects={savedObjects}
-        dataSourceEnabled={!!dataSource}
-        hideLocalCluster={dataSource?.hideLocalCluster || false}
-        dataSourceManagement={dataSourceManagement}
-      />
-    ) : (
-      <EnhancedDataImporterPluginApp
-        basename={appBasePath}
-        notifications={notifications}
-        http={http}
-        navigation={navigation}
-        config={config}
-        savedObjects={savedObjects}
-        dataSourceEnabled={!!dataSource}
-        hideLocalCluster={dataSource?.hideLocalCluster || false}
-        dataSourceManagement={dataSourceManagement}
-      />
-    ),
+    <RedesignedDataImporterPluginApp
+      basename={appBasePath}
+      notifications={notifications}
+      http={http}
+      application={application}
+      navigation={navigation}
+      config={config}
+      savedObjects={savedObjects}
+      dataSourceEnabled={!!dataSource}
+      hideLocalCluster={dataSource?.hideLocalCluster || false}
+      dataSourceManagement={dataSourceManagement}
+    />,
     element
   );
 
